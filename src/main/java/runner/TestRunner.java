ddc55f7ff940ecad309d4446eaa7fea2897f1d20
Manoj RG <manojrg86@gmail.com>
2015-09-12 11:33:02 -0500

    Changes for Jenkins

---
+++ src/main/java/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package runner;
+
+import java.io.File;
+import java.util.List;
+
+import org.testng.TestNG;
+import org.testng.collections.Lists;
+
+public class TestRunner {
+
+	public static void main(String args[]){
+		File testNgXml=new File("testng.xml");
+		List<String> runFiles=Lists.newArrayList();
+		runFiles.add(testNgXml.getAbsolutePath());
+		TestNG testNG = new TestNG();
+		testNG.setTestSuites(runFiles);
+		testNG.run();
+	}
+}
